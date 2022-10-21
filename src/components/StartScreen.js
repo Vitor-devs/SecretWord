fd464c7b9f5d39b421332afda302203d913855d8
Vitor-devs <vitorcoelho2004@outlook.com>
2022-10-21 15:22:08 -0300

    Criado projeto

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>Clique no botão abaixo para inicio!</p>
+        <button>Começar game</button>
+    </div>
+  )
+}
+
+export default StartScreen;
